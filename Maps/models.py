3277a53d8e1e4e49a9b20d2c3516ef0555ac56d1
Patricia Mae <53422124+patdzn08@users.noreply.github.com>
2021-11-05 13:34:36 +0000

    added design

--- Maps/models.py
+++ Maps/models.py
@@ -1,11 +1,3 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
 
 # Create your models here.
-
-class Search(models.Model):
-    Origin = models.CharField(max_length=100)
-    destination = models.CharField(max_length=100)
-    date_searched= models.DateTimeField(default=timezone.now)
-    metric=models.BooleanField(default=False)
--- Maps/views.py
+++ Maps/views.py
@@ -1,50 +1,51 @@
 from django.shortcuts import render
 import urllib.parse
 import requests
-from django.contrib import messages
 main_api = "https://www.mapquestapi.com/directions/v2/route?" 
 key = "yPfDRcQKBMOkOjMAG3WtXIzcCmmI0t5j"
 
 
 
 def home(request):
+    #Input Directions
     InputOrig=request.POST.get('inputlocation')
     InputDest=request.POST.get('inputDestination')
-
-    if (InputDest and InputOrig is not None):
-        url = main_api + urllib.parse.urlencode({"key": key, "from":InputOrig, "to":InputDest})
-        json_data = requests.get(url).json()
-        json_status = json_data["info"]["statuscode"]
-
-        if json_status == 0:
-            print("API Status: " + str(json_status) + " = A successful route call.\n")
-            directions=[]
-            Trip_Duration=json_data["route"]["formattedTime"] 
-            #Distance in KM
-            KM=str("{:.2f}".format((json_data["route"]["distance"])*1.61))
-            #Fuel in Liters
-            Fuel=str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78))
-            Manuever=json_data["route"]["legs"][0]["maneuvers"]
-            if (InputDest and InputOrig is not None):
-                for each in Manuever:
-                    directions.append(each["narrative"]+ " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
-            
-            
-            data={
-                "InputOrig":InputOrig,
-                "InputDest":InputDest,
-                "KM":KM,
-                "Trip_Duration":Trip_Duration,
-                "Fuel": Fuel,
-                "directions":directions
-            }
-
-            
-        
     
+    #MapQuest
+    url = main_api + urllib.parse.urlencode({"key": key, "from":InputOrig, "to":InputDest})
+    json_data = requests.get(url).json()
+    json_status = json_data["info"]["statuscode"]
+
+
+
+    if json_status == 0:
+        print("API Status: " + str(json_status) + " = A successful route call.\n")
+        directions=[]
+        #Detination Time
+        Trip_Duration=json_data["route"]["formattedTime"] 
+        #Distance in KM
+        KM=str("{:.2f}".format((json_data["route"]["distance"])*1.61))
+        #Fuel in Liters
+        Fuel=str("{:.2f}".format((json_data["route"]["fuelUsed"])*3.78))
+        #Directions
+        Manuever=json_data["route"]["legs"][0]["maneuvers"]
+        if (InputDest and InputOrig is not None):
+            for each in Manuever:
+                directions.append(each["narrative"]+ " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+        
+        
+        data={
+            "InputOrig":InputOrig,
+            "InputDest":InputDest,
+            "KM":KM,
+            "Trip_Duration":Trip_Duration,
+            "Fuel": Fuel,
+            "directions":directions
+        }
     
         return render(request, 'Maps/home.html',data)
 
 
 def about(request):
     return render(request, 'Maps/about.html', {'title': 'About'})
+
